21eba32657cca7a19019c82d5939040170236718
sayyyyyy <190781001@ccmailg.meijo-u.ac.jp>
2021-09-08 07:33:41 +0900

    アニメーション追加

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ window.onload = function() {
   const loader = document.getElementById('loading');
   loader.classList.add('completed');
 }
+
+$(function() {
+  $(window).scroll(function() {
+    $(".chapter").each(function() {
+      var scroll = $(window).scrollTop();
+      var blockPosition = $(this).offset().top;
+      var windowHeihgt = $(window).height();
+      if (scroll > blockPosition - windowHeihgt + 300) {
+        $(this).addClass("blockIn");
+      }
+    });
+  });
+});
