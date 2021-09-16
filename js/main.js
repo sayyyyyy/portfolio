d4a09b37f13bd1efecadac07a803a80a0596278e
青木聖弥 <aokikiyoshiwataru@aokikiyoshiwatarunoMacBook-Pro.local>
2021-09-16 22:00:34 +0900

    色々変更

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
+//mobileサイズ用のメニュー
 $(function(){
   $('#menu-btn').click(function(){
     $('.main-nav').toggleClass('is-active');
   });
 });
 
+//ローディング画面
 window.onload = function() {
   const loader = document.getElementById('loading');
   loader.classList.add('completed');
 }
 
+//スクロールしたらアニメーション
 $(function() {
   $(window).scroll(function() {
-    $(".chapter").each(function() {
+    $(".animate--add").each(function() {
       var scroll = $(window).scrollTop();
       var blockPosition = $(this).offset().top;
       var windowHeihgt = $(window).height();
       if (scroll > blockPosition - windowHeihgt + 300) {
         $(this).addClass("blockIn");
+      }
+    });
+  });
+  $(window).scroll(function() {
+    $(".page-header").each(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll > 100) {
+        $(this).addClass("blockIn");
+      } 
+      else {
+        $(this).removeClass("blockIn");
       }
     });
   });
